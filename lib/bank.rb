1ff244cd8b11e12f7474bcfdb79b91513640e3c8
Rafael Jesus <rafaelljesus86@gmail.com>
2016-07-25 11:20:46 -0300

    Fix require

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,8 +1,8 @@
+require_relative '../config/setup'
+
 require 'bank/version'
 require 'bank/models/account'
 require 'bank/models/user'
-
-require_relative '../config/setup'
 
 module Bank
   module Models
